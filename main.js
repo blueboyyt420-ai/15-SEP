f68319f40c5e5f2b6d19c5619055cbfe6fd7c096
RAMESHREDDY32941 <blueboyyt420@gmail.com>
2025-11-07 11:24:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+let dog={
+    "name":"tommy",weight:2.5,color:"white"
+}
+console.log(typeof(dog));
+console.log(dog);
+dog.height=2
+console.log(dog);
+dog.breed="bulldog"
+dog.name="rocky"
+console.log(dog);
+class Car {
+  constructor(name, year) {
+    this.name = name;
+    this.year = year;
+  }
+}
+const myCar1= new Car("ford",2014);
+const myCar2= new Car("audi",2019);
+const myCar3= new Car("bmw",2020);
+console.log(myCar3);
+console.log(myCar2);
+console.log(myCar1);
+console.log(typeof(myCar1));
